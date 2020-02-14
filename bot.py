967075f47216efc0a4c2b8886523aac0d53ed371
aylingumus <aylingumus7@gmail.com>
2020-02-14 17:00:21 +0300

    steam api endpoint variables are refactored.  bot is declared as a global variable.

--- bot.py
+++ bot.py
@@ -1,17 +1,18 @@
+import os
 import time
 import schedule
 import requests
 import telegram
 from telegram import ParseMode
 
+global bot = telegram.Bot(os.environ.get('BOT_TOKEN')) # Telegram Bot Authorization Token
 
-def telegram_bot_send_message(bot_message):
-    bot = telegram.Bot('<TOKEN>') # Telegram Bot Authorization Token
+def telegram_bot_send_message(bot_message):    
     bot_chatID = bot.get_updates()[0].message.chat_id
     bot.sendMessage(bot_chatID, bot_message, parse_mode=ParseMode.HTML)
 
 def notify():
-    your_wishlist = get_wishlist('<STEAM_USER_ID>') # Steam User ID
+    your_wishlist = get_wishlist(os.environ.get('STEAM_USER_ID')) # Steam User ID
     games_on_sale = get_games_on_sale(your_wishlist)
     # If there is a game in Wishlist with discount 50% or more, sends game sale info to user
     if len(games_on_sale) > 0:
@@ -24,7 +25,7 @@ def notify():
 
 # Gets wishlist with Steam API
 def get_wishlist(steam_user_id):
-    wishlist_endpoint = 'https://store.steampowered.com/wishlist/id/' + steam_user_id + '/wishlistdata/'
+    wishlist_endpoint = f"https://store.steampowered.com/wishlist/id/{steam_user_id}/wishlistdata/"
     wishlist = requests.get(wishlist_endpoint).json() 
     return wishlist
 
@@ -32,7 +33,7 @@ def get_wishlist(steam_user_id):
 def get_games_on_sale(wishlist):    
     games_on_sale = []
     for appid in wishlist:
-        app_details_endpoint = 'https://store.steampowered.com/api/appdetails?appids=' + appid
+        app_details_endpoint = f"https://store.steampowered.com/api/appdetails?appids={appid}"
         app_details = requests.get(app_details_endpoint).json()
         app = app_details[appid]['data']
         discount_filter(app, games_on_sale)
